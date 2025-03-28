eaa7e78d8b2d95065a3de29a4b2d2f05f4a1eb74
NRTofficial4 <nrtofficial44@gmail.com>
2025-03-28 20:50:34 +0100

    manyChangges

--- My1Server.ino
+++ My1Server.ino
@@ -11,9 +11,51 @@ const char* password = "12341234";
 IPAddress local_IP(192,168,10,1);
 IPAddress subnet(255,255,255,0);
 
-#define LED_PIN D10 
-
 ESP8266WebServer server(80);
+
+
+#define LED_G D13
+#define LED_Y D12
+#define LED_R D11
+
+class LED{
+    public:
+
+    //htpp adresses of each json endpoint to turn on/off led
+    const char* led_on;
+    const char* led_off;
+
+    int led_pin;
+
+      LED(int led_pin1, const char* led_off1, const char* led_on1){
+        led_pin = led_pin1;
+        led_on = led_on1;
+        led_off = led_off1;
+        };
+};
+
+LED Led_g(LED_G,"/led_g_off","/led_g_on");
+LED Led_r(LED_R,"/led_r_off","/led_r_on");
+LED Led_y(LED_Y,"/led_y_off","/led_y_on");
+
+void TurnRoff(){ server.on(Led_r.led_off,HTTP_GET,[](){digitalWrite(LED_R,LOW);}); }
+void TurnYoff(){ server.on(Led_y.led_off,HTTP_GET,[](){digitalWrite(LED_Y,LOW);}); }
+void TurnGoff(){ server.on(Led_g.led_off,HTTP_GET,[](){digitalWrite(LED_G,LOW);}); }
+
+void TurnRon(){ server.on(Led_r.led_on,HTTP_GET,[](){digitalWrite(LED_R,HIGH);}); }
+void TurnYon(){ server.on(Led_y.led_on,HTTP_GET,[](){digitalWrite(LED_Y,HIGH);}); }
+void TurnGon(){ server.on(Led_g.led_on,HTTP_GET,[](){digitalWrite(LED_G,HIGH);}); }
+
+void LedControl(){
+
+  TurnRoff();
+  TurnGoff();
+  TurnYoff();
+
+  TurnRon();
+  TurnGon();
+  TurnYon();
+}
 
 void HandleRoot(){
 
@@ -27,16 +69,7 @@ void HandleRoot(){
   file.close();
 }
 
-void TurnOnLed(){
-  digitalWrite(LED_PIN,HIGH);
-}
-
-void TurnOffLed(){
-  digitalWrite(LED_PIN,LOW);
-}
-
 void setup() {
-
 
   Serial.begin(115200);
   
@@ -51,14 +84,15 @@ void setup() {
     Serial.print("SPIFFS not init!");
     return;
   }
+  
   server.on("/",HTTP_GET,HandleRoot);
 
-  server.on("/turn_on_led",HTTP_GET,TurnOnLed);
-
-  server.on("/turn_off_led",HTTP_GET,TurnOffLed);
+  LedControl();
 
 
-  pinMode(LED_PIN,OUTPUT);
+  pinMode(LED_G,OUTPUT);
+  pinMode(LED_R,OUTPUT);
+  pinMode(LED_Y,OUTPUT);
   server.begin();
 }
 
